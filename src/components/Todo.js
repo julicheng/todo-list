0eaaac6f4a6fe60f2811e67097efa5c83ee2dd29
Juli Cheng <julicheng@julis-mbp.lan>
2019-09-08 21:38:40 +0100

    Move completed todos style to css file

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 function Todo(props) {
-  const completedStyle = {
-    fontStyle: "italic",
-    textDecoration: "line-through",
-    color: "gray"
-  };
   return (
     <div className="todo">
       <input
@@ -13,7 +8,7 @@ function Todo(props) {
         checked={props.data.completed}
         onChange={() => props.handleChange(props.data.id)}
       />
-      <p style={props.data.completed ? completedStyle : null}>
+      <p className={props.data.completed ? "completedStyle" : null}>
         {props.data.text}
       </p>
     </div>
